0c2eee5c3b4998b37938bc4e2bb84a6719baad59
zulux99 <szymon14059@gmail.com>
2021-11-23 11:25:33 +0100

    obiektowe java zad 1-3

---
+++ Java/src/Zadania_obiektowe/zadanie1-3.java
@@ -0,0 +1,35 @@
+package Zadania_obiektowe;
+
+public class zadanie1 {
+    public class Komputer {
+        String plyta_glowna;
+        String procesor;
+        String karta_graficzna;
+        Integer ilosc_ram;
+        String obudowa;
+        Float pojemnosc_dysku;
+        public Komputer(String plyta_glowna, String procesor, String karta_graficzna, Integer ilosc_ram, String obudowa, Float pojemnosc_dysku) {
+            this.plyta_glowna = plyta_glowna;
+            this.procesor = procesor;
+            this.karta_graficzna = karta_graficzna;
+            this.ilosc_ram = ilosc_ram;
+            this.obudowa = obudowa;
+            this.pojemnosc_dysku = pojemnosc_dysku;
+        }
+        public Komputer(String plyta_glowna, String procesor, Integer ilosc_ram, String obudowa, Float pojemnosc_dysku) {
+            this.plyta_glowna = plyta_glowna;
+            this.procesor = procesor;
+            this.ilosc_ram = ilosc_ram;
+            this.obudowa = obudowa;
+            this.pojemnosc_dysku = pojemnosc_dysku;
+        }
+        public Komputer(String plyta_glowna, String procesor) {
+            this.plyta_glowna = plyta_glowna;
+            this.procesor = procesor;
+        }
+        public Komputer(String plyta_glowna, String procesor, Integer ilosc_ram) {
+            this(plyta_glowna, procesor);
+            this.ilosc_ram = ilosc_ram;
+        }
+    }
+}
